6f8be108dbb795b0bff7f81610c419281b63c5e8
Onur Kuşbabali <okusbabali@hotmail.com>
2020-12-26 15:27:42 +0300

    Database connection  created

--- AnimalShelterApp/Startup.cs
+++ AnimalShelterApp/Startup.cs
@@ -1,9 +1,11 @@
+using AnimalShelterApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +26,9 @@ namespace AnimalShelterApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<DbContextAnimalShelter>(opt => 
+                opt.UseSqlServer((Configuration.GetConnectionString("DefaultSql"))
+            ));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
